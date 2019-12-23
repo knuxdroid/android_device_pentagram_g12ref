f073c8909249c895333e0d4c8de9a0512624fa95
knuxify <knuxfanwin8.mail@gmail.com>
2019-12-23 10:58:32 +0100

    g12ref: add some values and init scripts from boot.img

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,19 +3,28 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/pentagram/g12ref/BoardConfigVendor.mk
 
-TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a9
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_VFP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := neon
+ARCH_ARM_HAVE_NEON := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_ARMV7A := true
 
+TARGET_BOARD_PLATFORM := meson6
 TARGET_BOOTLOADER_BOARD_NAME := g12ref
+TARGET_NO_BOOTLOADER := true
+# TARGET_NO_KERNEL := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_SIMULATOR := false
+TARGET_PROVIDES_INIT_RC := true
 
-#BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_CMDLINE := console=ttyS0,115200 loglevel=8 #rw init=/init loglevel=8
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 
@@ -28,6 +37,17 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/pentagram/g12ref/kernel
 
-BOARD_HAS_NO_SELECT_BUTTON := true
+#BOARD_HAS_NO_SELECT_BUTTON := true
 
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
+
+TARGET_RECOVERY_INITRC := device/pentagram/g12ref/rootdir/init.rc
+TARGET_RECOVERY_FSTAB := device/pentagram/g12ref/recovery.fstab
+
+DEVICE_RESOLUTION := 1024x768
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+
+#TW_FLASH_FROM_STORAGE := true
+#TW_CUSTOM_POWER_BUTTON := 116
+#TW_NO_REBOOT_BOOTLOADER := true
+#TWHAVE_SELINUX := true
